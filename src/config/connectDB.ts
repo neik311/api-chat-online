8fb484b4679ae229230a1bf51dda00d3dde6341e
nvkien123 <vankienn857@gmail.com>
2023-06-04 15:03:05 +0700

    add database

--- src/config/connectDB.ts
+++ src/config/connectDB.ts
@@ -1,14 +1,13 @@
-import {Sequelize} from "sequelize"
+import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('chatonline','root', '123456', {
-    host: 'localhost',
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    },
-
+const sequelize = new Sequelize("chatonline", "root", "123456", {
+  host: "localhost",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
 });
 
-export default sequelize
+export default sequelize;
